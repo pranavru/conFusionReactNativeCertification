1ed3fe7c59b4beecd73064d4cd53f6ef072151b1
Pranav Ruparelia <pranavruparelia9@gmail.com>
2020-04-07 13:39:19 -0400

    Delete Favorites

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -4,6 +4,8 @@ import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import Swipeout from 'react-native-swipeout';
+import { deleteFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -11,6 +13,10 @@ const mapStateToProps = state => {
         favorites: state.favorites
     }
 }
+
+const mapDispatchToProps = dispatch => ({
+    deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId))
+})
 
 class Favorites extends Component {
 
@@ -21,17 +27,27 @@ class Favorites extends Component {
         const { navigate } = this.props.navigation;
         const renderMenuItem = ({ item, index }) => {
 
+            const rightButton = [
+                {
+                    text: 'Delete', 
+                    type: 'delete',
+                    onPress: () => this.props.deleteFavorite(item.id)
+                }
+            ];
+
             return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    hideChevron={true}
-                    onPress={() => navigate('Dishdetail', { dishId: item.id })}
-                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
-                />
+                <Swipeout right={rightButton} autoClose={true}>
+                    <ListItem
+                        key={index}
+                        title={item.name}
+                        subtitle={item.description}
+                        hideChevron={true}
+                        onPress={() => navigate('Dishdetail', { dishId: item.id })}
+                        leftAvatar={{ source: {uri: baseUrl + item.image}}}
+                        />
+                </Swipeout>
             );
-        };
+        }
 
         if (this.props.dishes.isLoading) {
             return (
@@ -48,7 +64,7 @@ class Favorites extends Component {
         else {
             if (this.props.dishes.dishes === []) {
                 return (
-                    <View style={{ flex :1, marginVertical: '5%', marginHorizontal: '5%', justifyContent: 'center', alignSelf: "center" }}>
+                    <View style={{ flex: 1, marginVertical: '5%', marginHorizontal: '5%', justifyContent: 'center', alignSelf: "center" }}>
                         <Text>Currently there are no favorites added.</Text>
                     </View>
                 )
@@ -65,4 +81,4 @@ class Favorites extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
